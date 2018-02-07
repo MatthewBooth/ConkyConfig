1da125865366eee83b1033f92e7031d01f0cfd33
Matt Booth <kryten2k35@ultimarom.com>
2018-02-07 09:00:59 +0000

    Temperature colour scale

--- bin/temps.py
+++ bin/temps.py
@@ -24,7 +24,7 @@ def __get_gpu_temps():
             if device_count > 0:
                 for i in range(device_count):
                     gpu_temp = nvml.nvmlDeviceGetTemperature(nvml.nvmlDeviceGetHandleByIndex(i), 0)
-                    print(' GPU %(i)s: ${alignr}%(color)s%(temp)s${color}°C' % {
+                    print(' GPU %(i)s: ${alignr}${color %(color)s}%(temp)s${color}°C' % {
                         'i': i,
                         'color': get_temperature_color(gpu_temp),
                         'temp': gpu_temp
@@ -37,14 +37,14 @@ def __get_cpu_temps():
     cpu_temp = int(temps_list[0].current)
 
     print('$stippled_hr\nTemperatures\n$stippled_hr\n')
-    print('CPU: ${alignr}%(color)s%(temp)s${color}°C' % {
+    print('CPU: ${alignr}${color %(color)s}%(temp)s${color}°C' % {
         'color': get_temperature_color(cpu_temp),
         'temp': cpu_temp
     })
 
     for item in range(1, 5):
         core_temp = int(temps_list[item].current)
-        print(' Core %(i)s: ${alignr}%(color)s%(temp)s${color}°C' % {
+        print(' Core %(i)s: ${alignr}${color %(color)s}%(temp)s${color}°C' % {
             'i': item,
             'color': get_temperature_color(core_temp),
             'temp': core_temp
