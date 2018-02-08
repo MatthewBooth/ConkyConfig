c0caa513ca0d185aa8a1b1c7cd94816d4f826f6d
Matt Booth <kryten2k35@ultimarom.com>
2018-02-08 10:11:02 +0000

    Because Colour

--- bin/color_scale.py
+++ bin/color_scale.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-from colour import Color
+from colour import Color as Colour
 
 
 def get_scaled_color(value):
-    start_color = Color('green')
-    end_color = Color('red')
+    start_color = Colour('white')
+    end_color = Colour('red')
     colors = list(start_color.range_to(end_color, 101))
 
     value = int(value)
@@ -14,10 +14,10 @@ def get_scaled_color(value):
 
 
 def get_temperature_color(value):
-    start_color = Color('green')
-    end_color = Color('red')
-    colors = list(start_color.range_to(end_color, 101))
+    start_color = Colour('green')
+    end_color = Colour('red')
+    colors = list(start_color.range_to(end_color, 70))
 
-    value = int(value)
+    value = int(value - 20)
 
     return colors[value]
