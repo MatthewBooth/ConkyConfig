2f4b1429a1488abfd6e0be49ba33d176b4764da6
Matt Booth <kryten2k35@ultimarom.com>
2018-02-21 09:32:44 +0000

    Tweaked the config layout a little bit

--- conky_config.lua
+++ conky_config.lua
@@ -7,50 +7,39 @@ conky.config = {
     own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
     own_window_transparent = false,
     own_window_argb_visual = true,
-    own_window_argb_value = 192,
+    own_window_argb_value = 128,
     own_window_colour = '000000',
-
-
-    double_buffer = true,
-    background = true,
-    stippled_borders = 0,
-
-	-- Window Position
 	minimum_height = 300,
 	maximum_width = 300,
-
 	alignment = 'top_right',
 	gap_x = 12,
 	gap_y = 35,
-
 	background = false,
-	cpu_avg_samples = 1,
+
+	-- System Configurations
+    double_buffer = true,
+    cpu_avg_samples = 1,
+	update_interval = 1,
+	total_run_times = 0,
+	no_buffers = true,
+	override_utf8_locale = true,
+
+	-- Styling Configurations
+	use_xft = true,
+	font = 'Roboto:size=7',
 	default_color = white,
 	default_shade_color = black,
+	default_bar_height = 3,
+	default_graph_height = 20,
 	draw_borders = false,
     draw_graph_borders = true,
     draw_outline = false,
     draw_shades = false,
-	use_xft = true,
-	font = 'Roboto:size=7',
-
-	update_interval = 1,
-	total_run_times = 0,
-
-
-	
-	default_bar_height = 3,
-	default_graph_height = 20,
-
-	no_buffers = true,
-	
-	override_utf8_locale = true,
+	stippled_borders = 0,
 	uppercase = true,
 	short_units = true,
 }
 
 conky.text = [[
-# Run the conky Script
-# Pass the network interface name as a parameter
-${execpi 1 ~/.conky/conky.py}
+	${execpi 1 ~/.conky/conky.py} # Run the conky Script
 ]]
